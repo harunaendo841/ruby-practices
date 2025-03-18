bfcb1729daff1a999311ca5a21815b741ecb99c1
harunaendo841 <h.runa0414@gmail.com>
2025-03-18 16:04:21 +0900

    -lオプションを追加し、詳細情報を表示できるようにした。

--- 04.ls/ls.rb
+++ 04.ls/ls.rb
@@ -2,6 +2,8 @@
 # frozen_string_literal: true
 
 require 'optparse'
+require 'etc'
+require 'time'
 
 MAX_COLUMNS = 3
 COLUMN_PADDING = 2
@@ -17,6 +19,37 @@ def generate_file_list(path, show_all, reverse)
   files.sort!
   files.reverse! if reverse
   files
+end
+
+def file_details(file_name, path)
+  stat = File.lstat(File.join(path, file_name))
+
+  format(
+    '%<mode>s %<nlink>2d %-<owner>8s %-<group>8s %<size>6d %<mtime>s %<name>s',
+    mode: format_mode(stat.mode),
+    nlink: stat.nlink,
+    owner: Etc.getpwuid(stat.uid).name,
+    group: Etc.getgrgid(stat.gid).name,
+    size: stat.size,
+    mtime: stat.mtime.strftime('%b %d %H:%M'),
+    name: file_name
+  )
+end
+
+def format_mode(mode)
+  type = case mode & 0o170000
+         when 0o040000 then 'd'
+         when 0o100000 then '-'
+         when 0o120000 then 'l'
+         when 0o020000 then 'c'
+         when 0o060000 then 'b'
+         when 0o140000 then 's'
+         else '?'
+         end
+
+  perms = (0..8).map { |i| (mode & (1 << (8 - i))).zero? ? '-' : 'rwxrwxrwx'[i] }.join
+
+  type + perms
 end
 
 def format_file_table(file_list, columns)
@@ -48,20 +81,25 @@ def print_file_table(table, column_widths, padding)
 end
 
 def main
-  options = { show_all: false, reverse: false }
+  options = { show_all: false, reverse: false, detailed_view: false }
   OptionParser.new do |opts|
     opts.on('-a', '隠しファイルを表示') { options[:show_all] = true }
     opts.on('-r', 'ファイルを逆順で表示') { options[:reverse] = true }
+    opts.on('-l', '詳細情報を表示') { options[:detailed_view] = true }
   end.parse!
 
   path = ARGV.first || '.'
-
   validate_directory_path(path)
 
   file_list = generate_file_list(path, options[:show_all], options[:reverse])
-  file_table = format_file_table(file_list, MAX_COLUMNS)
-  column_widths = calculate_column_widths(file_table)
-  print_file_table(file_table, column_widths, COLUMN_PADDING)
+
+  if options[:detailed_view]
+    puts(file_list.map { |file| file_details(file, path) })
+  else
+    file_table = format_file_table(file_list, MAX_COLUMNS)
+    column_widths = calculate_column_widths(file_table)
+    print_file_table(file_table, column_widths, COLUMN_PADDING)
+  end
 end
 
 main
