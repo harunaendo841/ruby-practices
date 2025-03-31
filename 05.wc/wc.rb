088ce6f3d12c5cf3af747e3d7a0c20da88910213
harunaendo841 <h.runa0414@gmail.com>
2025-03-31 10:28:11 +0900

    countsおよびtotalの集計方法を配列からハッシュに変更し、可読性を向上

--- 05.wc/wc.rb
+++ 05.wc/wc.rb
@@ -3,6 +3,8 @@
 
 require 'optparse'
 
+COUNT_ORDER = %i[lines words bytes]
+
 options = { lines: false, words: false, bytes: false }
 OptionParser.new do |opts|
   opts.on('-l') { options[:lines] = true }
@@ -14,24 +16,25 @@ options[:lines] = options[:words] = options[:bytes] = true if options.values.non
 
 def count_file_data(io)
   data = io.read
-  lines = data.count("\n")
-  words = data.split(/\s+/).count { |word| !word.empty? }
-  bytes = data.bytesize
-  [lines, words, bytes]
+  {
+    lines: data.count("\n"),
+    words: data.split(/\s+/).count { |word| !word.empty? },
+    bytes: data.bytesize
+  }
 end
 
 def format_counts(counts, filename, options, padding)
   formatted = []
-  formatted << counts[0].to_s.rjust(padding) if options[:lines]
-  formatted << counts[1].to_s.rjust(padding) if options[:words]
-  formatted << counts[2].to_s.rjust(padding) if options[:bytes]
+  COUNT_ORDER.each do |key|
+    formatted << counts[key].to_s.rjust(padding) if options[key]
+  end
   formatted << filename unless filename.empty?
   formatted.join(' ')
 end
 
 filenames = ARGV
 all_counts = []
-total = [0, 0, 0]
+total = { lines: 0, words: 0, bytes: 0 }
 
 if filenames.empty?
   counts = count_file_data($stdin)
@@ -39,20 +42,19 @@ if filenames.empty?
   filenames = ['']
   total = counts
 else
-
   filenames.each do |filename|
     abort("Error: ファイル '#{filename}' が存在しません。") unless File.exist?(filename)
     abort("Error: '#{filename}' はファイルではありません。") unless File.file?(filename)
 
     File.open(filename) do |file|
       counts = count_file_data(file)
-      total = total.zip(counts).map { |a, b| a + b }
+      COUNT_ORDER.each { |key| total[key] += counts[key] }
       all_counts << counts
     end
   end
 end
 
-padding = (all_counts + [total]).flatten.map(&:to_s).map(&:length).max
+padding = (all_counts + [total]).flat_map { |c| COUNT_ORDER.map { |key| c[key].to_s.length } }.max
 
 filenames.each_with_index do |filename, i|
   puts format_counts(all_counts[i], filename, options, padding)
